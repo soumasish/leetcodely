ad01b56dd8450fea7cb3a30b1348a4c9e76930b2
Soumasish Goswami <sgoswami@yahoo-inc.com>
2017-08-01 11:41:57 -0700

    updated maximum subarray sum

--- src/53_maximum_subarray.py
+++ src/53_maximum_subarray.py
@@ -5,19 +5,19 @@ For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
 the contiguous subarray [4,-1,2,1] has the largest sum = 6."""
 import sys
 
+
 class Solution(object):
     def maxSubArray(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        curr_sum, max_sum = nums[0], nums[0]
-        for i in range(1, len(nums)):
-            if curr_sum + nums[i] > 0:
-                curr_sum += nums[i]
-            else:
-                curr_sum = 0
+        curr_sum, max_sum = 0, -sys.maxsize
+        for i in nums:
+            curr_sum += i
             max_sum = max(curr_sum, max_sum)
+            if curr_sum < 0:
+                curr_sum = 0
         return max_sum
 
 if __name__ == '__main__':
