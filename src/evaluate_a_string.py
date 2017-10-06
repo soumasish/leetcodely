f0adf94225ebe7f63905e30bf5cdf643d56077a8
Soumasish Goswami <sgoswami@yahoo-inc.com>
2017-10-05 22:37:47 -0700

    first working solution to evaluate a string

--- src/evaluate_a_string.py
+++ src/evaluate_a_string.py
@@ -3,7 +3,6 @@ import re
 import collections
 
 
-
 class Solution:
     def evaluate(self, s: str) -> int:
         expr = [i for i in re.split(r'(\d+|\W+)', s) if i]
@@ -11,8 +10,27 @@ class Solution:
         return self.evalPostfix(rpn)
 
     def convertToPostfix(self, infix: list) -> list:
-        pass
+        stack = collections.deque()
+        result = []
+        for item in infix:
+            if item.isdigit():
+                result.append(item)
+            else:
+                while len(stack) > 0 and self.has_higher_precedence(stack[-1], item):
+                    result.append(stack.pop())
+                result.append(item)
+        while len(stack) > 0:
+            result.append(stack.pop())
+        return result
 
+    def has_higher_precedence(self, a: str, b: str) -> bool:
+        if a == '/' and b == '*' or b == '+' or b == '-':
+            return True
+        if a == '*' and b == '+' or '-':
+            return True
+        if a == '+' and b == '-':
+            return True
+        return False
 
     def evalPostfix(self, p: list) -> int:
         stack = collections.deque()
@@ -34,3 +52,7 @@ class Solution:
                     total = prev // curr
                 stack.append(total)
         return stack.pop()
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.evaluate('3+2-7/4*3'))
