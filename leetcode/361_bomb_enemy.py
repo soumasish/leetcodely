834675d5640c59bdd8ea3df709c21a78e5faf4bf
Soumasish <soumasish@gmail.com>
2017-12-21 12:31:28 -0800

    A bunch of outliers, a more readable permutations, a better n-queens.

--- leetcode/361_bomb_enemy.py
+++ leetcode/361_bomb_enemy.py
@@ -3,6 +3,7 @@ enemies you can kill using one bomb. The bomb kills all the enemies in the same 
 until it hits the wall since the wall is too strong to be destroyed.
 Note that you can only put the bomb at an empty cell."""
 
+
 class Solution:
     def maxKilledEnemies(self, grid):
         """
@@ -12,23 +13,44 @@ class Solution:
         max_kills = 0
         for i in range(len(grid)):
             for j in range(len(grid[i])):
-                if grid[i][i] == '0':
+                if grid[i][j] == '0':
                     kills = self.total_kills(i, j, grid)
                     max_kills = max(max_kills, kills)
         return max_kills
 
     def total_kills(self, row, col, grid):
         count = 0
-        #col kill
-        for i in range(len(grid)):
+        i = col
+        while i >= 0:
             if grid[row][i] == 'E':
                 count += 1
-            if grid[row][i] == 'W':
+            elif grid[row][i] == 'W':
                 break
+            i -= 1
+        i = col
+        while i < len(grid[0]):
+            if grid[row][i] == 'E':
+                count += 1
+            elif grid[row][i] == 'W':
+                break
+            i += 1
+        i = row
+        while i >= 0:
+            if grid[i][col] == 'E':
+                count += 1
+            elif grid[i][col] == 'W':
+                break
+            i -= 1
+        i = row
+        while i < len(grid):
+            if grid[i][col] == 'E':
+                count += 1
+            elif grid[i][col] == 'W':
+                break
+            i += 1
+        return count
 
-        for j in range(len(grid)):
-            pass
 
-
-
-
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.maxKilledEnemies([["0","E","0","0"],["E","0","W","E"],["0","E","0","0"]]))
