cd11f3ec83c7df03d4174ff37be3ae2bf8f93df7
Soumasish Goswami <soumasish.goswami@samba.tv>
2019-07-18 02:34:57 -0700

    added a test

--- python/outliers/longest_series_of_consecutive_integers.py
+++ python/outliers/longest_series_of_consecutive_integers.py
@@ -13,21 +13,24 @@
 # In this example the answer is 3, both 2 and 3 have 4 adjacent occurences, but 3 is higher than 2, so the answer is 3.
 
 import collections
+import unittest
 
 
 class Solution:
+    def __init__(self):
+        self.neighbors = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        self.maxer = [-1, -1]
 
     def dfs(self, grid: [list]) -> int:
-        neighbors = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        maxer = [-1, -1]
 
         def helper(row, col, num, visited, counter):
             visited[row][col] = True
             counter.append(1)
 
-            for neighbor in neighbors:
+            for neighbor in self.neighbors:
                 row, col = row + neighbor[0], col + neighbor[1]
-                if 0 <= row < len(grid) and 0 <= col < len(grid[row]) and grid[row][col] == num and not visited[row][col]:
+                if 0 <= row < len(grid) and 0 <= col < len(grid[row]) and grid[row][col] == num and not visited[row][
+                    col]:
                     helper(row, col, num, visited, counter)
 
         for i in range(len(grid)):
@@ -35,25 +38,24 @@ class Solution:
                 vis = [[False for _ in range(len(grid[i]))] for i in range(len(grid))]
                 lst = []
                 helper(i, j, grid[i][j], vis, lst)
-                if len(lst) > maxer[1]:
-                    maxer[0], maxer[1] = grid[i][j], len(lst)
-                if len(lst) == maxer[1]:
-                    if maxer[0] < grid[i][j]:
-                        maxer[0] = grid[i][j]
-        return maxer[0]
+                if len(lst) > self.maxer[1]:
+                    self.maxer[0], self.maxer[1] = grid[i][j], len(lst)
+                if len(lst) == self.maxer[1]:
+                    if self.maxer[0] < grid[i][j]:
+                        self.maxer[0] = grid[i][j]
+        return self.maxer[0]
 
     def bfs(self, grid: [list]) -> int:
-        neighbors = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        maxer = [-1, -1]
 
         def bfs(queue, visited, num, counter):
             while len(queue) > 0:
                 row, col = queue.pop()
                 visited[row][col] = True
                 counter.append(1)
-                for neighbor in neighbors:
+                for neighbor in self.neighbors:
                     row, col = row + neighbor[0], col + neighbor[1]
-                    if 0 <= row < len(grid) and 0 <= col < len(grid[row]) and grid[row][col] == num and not visited[row][col]:
+                    if 0 <= row < len(grid) and 0 <= col < len(grid[row]) and grid[row][col] == num and not \
+                    visited[row][col]:
                         queue.appendleft((row, col))
 
         for i in range(len(grid)):
@@ -64,14 +66,26 @@ class Solution:
                 queue.appendleft((i, j))
                 bfs(queue, visited, grid[i][j], lst)
 
-                if len(lst) > maxer[1]:
-                    maxer[0], maxer[1] = grid[i][j], len(lst)
-                if len(lst) == maxer[1]:
-                    if maxer[0] < grid[i][j]:
-                        maxer[0] = grid[i][j]
-        return maxer[0]
+                if len(lst) > self.maxer[1]:
+                    self.maxer[0], self.maxer[1] = grid[i][j], len(lst)
+                if len(lst) == self.maxer[1]:
+                    if self.maxer[0] < grid[i][j]:
+                        self.maxer[0] = grid[i][j]
+        return self.maxer[0]
 
 
-solution = Solution()
-print(solution.dfs([[2, 3, 3, 1], [2, 3, 3, 2], [2, 2, 3, 5]]))
-print(solution.bfs([[2, 3, 3, 1], [2, 3, 3, 2], [2, 2, 3, 5]]))
+class TestSolution(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.grid = [[2, 3, 3, 1], [2, 3, 3, 2], [2, 2, 3, 5]]
+        self.solution = Solution()
+
+    def test_dfs(self):
+        self.assertEqual(self.solution.dfs(self.grid), 3)
+
+    def test_bfs(self):
+        self.assertEqual(self.solution.bfs(self.grid), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
