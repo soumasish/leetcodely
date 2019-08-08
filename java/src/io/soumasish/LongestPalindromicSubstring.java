4bc6aef8931d2cb076eb424750d4633ad0c7407c
Soumasish Goswami <soumasish.goswami@samba.tv>
2019-08-08 13:23:36 -0700

    Changing java directory structure.

--- java/src/io/soumasish/LongestPalindromicSubstring.java
+++ java/src/io/soumasish/LongestPalindromicSubstring.java
@@ -1,38 +1,40 @@
+package io.soumasish;
+
 import java.util.HashMap;
-import java.lang.*;
-class Solution {
+
+public class LongestPalindromicSubstring {
     public int lengthOfLongestSubstring(String s) {
-        
+
         HashMap<String, Integer> hmap = new HashMap<String, Integer>();
-       
+
         int max = 0;
         int longestCount = 0;
-        int start = 0; 
+        int start = 0;
         for(int i=0;i<s.length();i++){
             String ch = Character.toString(s.charAt(i));
             if(hmap.get(ch) == null){
                 hmap.put(ch, i);
                 longestCount++;
-                
-            } else{  
+
+            } else{
                 max = Math.max(max,longestCount);
-    
+
                 if (start <= hmap.get(ch) ){
                     int buff = i - hmap.get(ch);
 
                     if(buff<longestCount){
                         longestCount = buff;
-                    } 
+                    }
                     start = hmap.get(ch)+1;
                 }
                 else {
                     longestCount++;
                 }
-                
+
                 hmap.put(ch, i);
             }
             max = Math.max(max,longestCount);
-            
+
         }
         return max;
     }
--- java/src/io/soumasish/TwoSum.java
+++ java/src/io/soumasish/TwoSum.java
@@ -1,5 +1,9 @@
-class TwoSum {
-    //this Solution is accepted
+package io.soumasish;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer,Integer> numbers = new HashMap<Integer,Integer>();
         for(int i=0;i<nums.length;i++){
